c8c0e1ddbf5f2ddd919403a10b8d2fd2c00e1b17
thiagoecs <thiagoc@metropolia.fi>
2021-01-16 22:01:09 +0200

    adding again task B cause i forgot to push before

--- components/List.js
+++ components/List.js
@@ -1,21 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
-const url =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
 
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
 
-  const loadMedia = async () => {
+  const loadMedia = async (limit = 5) => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(baseUrl + 'media?limit=' + limit);
       const json = await response.json();
-      console.log(json);
-      setMediaArray(json);
-    } catch (error) {
-      console.error('loadMedia error', error);
+      console.log('response json data', json);
+
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const json = response.json();
+          return json;
+        })
+      );
+      console.log('media array data', media);
+      setMediaArray(media);
+    } catch (e) {
+      console.error('loadMedia error', e.message);
     }
   };
 
@@ -26,7 +34,7 @@ const List = () => {
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item, index) => index.toString()}
+      keyExtractor={(_item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
